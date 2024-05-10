eb8d4f41a7059a748706a676a01ed7233488fbd8
Qasim Fida <78118504+qasimfida@users.noreply.github.com>
2024-05-11 04:10:42 +0500

    testminial added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import "../src/App.css";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
-
-
 const SkewedImages = () => {
   const wrapperRef = useRef(null);
 
@@ -21,7 +19,6 @@ const SkewedImages = () => {
       skewSetter(clamp(velocity));
     };
 
-    
     wrapperRef.current.addEventListener("scroll", handleScroll);
 
     return () => {
@@ -29,27 +26,32 @@ const SkewedImages = () => {
     };
   }, []);
 
-
   const sectionVariant = {
     visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
-    hidden: { opacity: 0, scale: 0 }
+    hidden: { opacity: 0, scale: 0 },
   };
-    const control = useAnimation();
-    const [ref, inView] = useInView();
-    const [coursesText, setCoursesText] = useState("Start Course");
-  
-    useEffect(() => {
-      if (inView) {
-        control.start("visible");
-        setCoursesText("Courses");
-      } else {
-        control.start("hidden");
-        setCoursesText("Start Course");
-      }
-    }, [control, inView]);
-  
-  
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+  const [firstref, firstrefView] = useInView();
+  const [coursesText, setCoursesText] = useState(null);
+  const [refTestimonial, inViewTestimonial] = useInView();
+  // const [testimonialText, setTestimonialText] = useState();
 
+  useEffect(() => {
+    debugger
+    if (firstrefView && coursesText !== "Start Course") {
+
+      setCoursesText("Start Course");
+    } else if (inView && coursesText !== "Courses") {
+      setCoursesText("Courses" );
+    } else if(inViewTestimonial && coursesText  !== "test") {
+
+      setCoursesText("testmonial ");
+    }
+   
+  }, [ inView, firstrefView, inViewTestimonial]);
+
+  console.log(coursesText);
   return (
     <div
       id="wrapper"
@@ -60,7 +62,14 @@ const SkewedImages = () => {
         <div className="w-full h-full flex items-center justify-center ">
           <button className="btn-fixed">Get started</button>
         </div>
-        <div className="circle gradient-green">
+        <div className="circle gradient-green"
+        
+          ref={firstref}
+          animate={control}
+          variants={sectionVariant}
+          controls="hidden"
+
+        >
           <div className="flex flex-col items-center px-16 pt-7 pb-20 bg-slate-950 max-md:px-5">
             <div className="flex flex-col items-center w-full max-w-[1180px] max-md:max-w-full">
               <div className="flex gap-5 justify-between items-center self-stretch w-full max-md:flex-wrap max-md:max-w-full">
@@ -95,7 +104,10 @@ const SkewedImages = () => {
                 </div>
               </div>
               <div className="mt-20 text-[16px] font-bold text-blue-700 leading-[52.44px] max-md:mt-10 max-md:text-4xl">
-                <h1 aria-hidden="true" class="text-blue-700  text filter-text capitalize">
+                <h1
+                  aria-hidden="true"
+                  class="text-blue-700   filter-text capitalize"
+                >
                   {coursesText}
                 </h1>
               </div>
@@ -172,68 +184,83 @@ const SkewedImages = () => {
                     </Parallax>
                   </ParallaxProvider>
                 </section>
-
-                <motion.section
-                  className="w-full"
-                  ref={ref}
-                  variants={sectionVariant}
-                  initial="hidden"
-                  animate={control}
-                >
-                  <div className="h-[30vh]">
-                  <h1 className="text-center text-[46px] font-semibold capitalize">
-            Our<span className="text-[#194BFD]"> full {coursesText}</span>
-          </h1>
-                    <p className="pt-[25px] text-[#E6E6E682] font-medium text-[16px] text-end w-[66%] ">
-                      Amet minim mollit non deserunt ullamco est sit aliqua{" "}
-                      <br /> dolor do amet sint. Velit officia consequat duis
-                      enim velit <br />
-                    </p>
-                  </div>
-                  <div className="h-[100vh]">
-                    <img
-                      srcSet="/background.png"
-                      className="h-[77%] w-[100%]"
-                    />
-                  </div>
-                  <div className="h-[100vh] flex">
-                    <div
-                      className="w-[50%] h-[100%] bg-gradient-to-r from-[#000618] to-[#000D25] code code--wider code--left aos-init aos-animate"
-                      data-aos="fade-up"
-                      data-aos-anchor-placement="bottom-bottom"
-                    >
-                      <h1 className="text-[28px] font-[500] text-center pt-[88px]">
-                        “Courses are good and I recommend”
-                      </h1>
-                      <p className="pt-[40px] px-[55px] leading-9 font-normal text-[21px]">
-                        Amet minim mollit non deserunt ullamco est sit aliqua
-                        dolor do amet sint. Velit officia consequat duis enim
-                        velit mollit. Exercitation veniam consequat sunt nostrud
-                        amet.Amet minim mollit non deserunt ullamco est sit
-                        aliqua dolor do amet sint. Velit officia consequat duis
-                        enim velit mollit. Exercitation veniam consequat sunt
-                        nostrud amet
-                      </p>
-                      <img
-                        className="w-[45%] h-[16%] ml-[42px] mt-10"
-                        src="sign.png"
-                      />
-                    </div>
-                    <div className="w-[50%] h-[100%] flex items-center justify-center">
-                      <div className="w-[80%] h-[80%] rounded-[33px]">
-                        <img className="rounded-[20px] filter grayscale hover:filter-none transition duration-500 ease-in-out" srcSet="/person.png" />
-                      </div>
-                    </div>
-                  </div>
-                </motion.section>
               </div>
             </div>
           </div>
         </div>
+        <motion.section
+          className="w-full"
+          ref={ref}
+          variants={sectionVariant}
+          animate={control}
+          controls="hidden"
+
+        >
+          <div className="h-[30vh]">
+            <h1 className="text-center text-[46px] font-semibold capitalize">
+              Our
+              <span className="text-[#194BFD]"> full {coursesText}</span>
+            </h1>
+            <p className="pt-[25px] text-[#E6E6E682] font-medium text-[16px] text-end w-[66%] ">
+              Amet minim mollit non deserunt ullamco est sit aliqua <br /> dolor
+              do amet sint. Velit officia consequat duis enim velit <br />
+            </p>
+          </div>
+          <div className="h-[100vh]">
+            <img srcSet="/background.png" className="h-[77%] w-[100%]" />
+          </div>
+        </motion.section>
+        <motion.section
+          className="w-full"
+          ref={refTestimonial}
+          variants={sectionVariant}
+          animate={control}
+        >
+          <div className="testmonial">
+            <h1 className="w-full h-screen">
+              <h1
+                aria-hidden="true"
+                class="text-blue-700  text filter-text capitalize"
+              >
+                {coursesText}
+              </h1>
+              <div className="h-[100vh] flex">
+                <div
+                  className="w-[50%] h-[100%] bg-gradient-to-r from-[#000618] to-[#000D25] code code--wider code--left aos-init aos-animate"
+                  data-aos="fade-up"
+                  data-aos-anchor-placement="bottom-bottom"
+                >
+                  <h1 className="text-[28px] font-[500] text-center pt-[88px]">
+                    “Courses are good and I recommend”
+                  </h1>
+                  <p className="pt-[40px] px-[55px] leading-9 font-normal text-[21px]">
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                    do amet sint. Velit officia consequat duis enim velit
+                    mollit. Exercitation veniam consequat sunt nostrud amet.Amet
+                    minim mollit non deserunt ullamco est sit aliqua dolor do
+                    amet sint. Velit officia consequat duis enim velit mollit.
+                    Exercitation veniam consequat sunt nostrud amet
+                  </p>
+                  <img
+                    className="w-[45%] h-[16%] ml-[42px] mt-10"
+                    src="sign.png"
+                  />
+                </div>
+                <div className="w-[50%] h-[100%] flex items-center justify-center">
+                  <div className="w-[80%] h-[80%] rounded-[33px]">
+                    <img
+                      className="rounded-[20px] filter grayscale hover:filter-none transition duration-500 ease-in-out"
+                      srcSet="/person.png"
+                    />
+                  </div>
+                </div>
+              </div>
+            </h1>
+          </div>
+        </motion.section>
       </div>
     </div>
   );
 };
-
 
 export default SkewedImages;
